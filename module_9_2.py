3650a420d3e0c42585ef3e5d09e33ac542f9ac6d
Strwolfy <strwolfy@gmail.com>
2024-12-03 23:11:10 +0300

    module_9_2.py

---
+++ module_9_2.py
@@ -0,0 +1,23 @@
+# Исходные данные
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+# Список длин строк из first_strings, если длина строки >= 5
+first_result = [len(s) for s in first_strings if len(s) >= 5]
+
+# Список пар слов (кортежей) одинаковой длины
+second_result = [
+    (f, s) for f in first_strings for s in second_strings if len(f) == len(s)
+]
+
+# Словарь строк и их длин, если длина строки четная
+third_result = {
+    s: len(s)
+    for s in first_strings + second_strings
+    if len(s) % 2 == 0
+}
+
+# Вывод результатов
+print(first_result)
+print(second_result)
+print(third_result)
